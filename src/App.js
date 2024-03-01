700fcf412008b5204922347270f07dd49e37a065
SeherKumsar <seherkumsar99@outlook.com>
2024-03-01 23:11:23 +0300

    added image to ProfileCard

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png';
 
-console.log(AlexaImage);
-console.log(CortanaImage);
-console.log(SiriImage); // large size image
-
 function App() {
     return (
         <div>
             <div>Personal Digital Assistans</div>
 
-            <img src="https://picsum.photos/200/300" />
-            {/* <img src={AlexaImage} />
-            <img src={SiriImage} /> */}
-
-            <ProfileCard title="Alexa" handle="@alexa99"/>
-            <ProfileCard title="Cortana" handle="@cortana32"/>
-            <ProfileCard title="Siri" handle="@siri01"/>
+            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
         </div>
     );
 }
