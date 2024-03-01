a34072af725c7be3ba0355a0a54bbf8983c6c5ad
SeherKumsar <seherkumsar99@outlook.com>
2024-03-01 22:23:07 +0300

    added props to ProfilCard

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,16 +1,8 @@
-function ProfileCard(props) {
-  // const title = props.title;
-  // const handle = props.handle;
-  const { title, handle } = props;
+function ProfileCard({title, handle}) {
+//   const { title, handle } = props;
   
   return (
     <div>
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
       <div>Title is {title}</div>
       <div>Handle is {handle}</div>
     </div>
