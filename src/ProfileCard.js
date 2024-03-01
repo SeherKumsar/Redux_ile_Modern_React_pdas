a0d72b7d3563d49037ece3cc25a404ba0f572e63
SeherKumsar <seherkumsar99@outlook.com>
2024-03-01 23:58:07 +0300

    added description to ProfileCard

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,4 +1,4 @@
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
   return (
     <div className="card">
       <div className="card-image">
@@ -9,9 +9,10 @@ function ProfileCard({ title, handle, image }) {
 
       <div className="card-content">
         <div className="media-content">
-          <p className="title is-4">Title is {title}</p>
-          <p className="subtitle is-6">Handle is {handle}</p>
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
         </div>
+        <div className="content">{description}</div>
       </div>
     </div>
   );
