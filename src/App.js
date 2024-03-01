5d0dc7b4bed18c423d692d8ac03733e0c0f69a06
SeherKumsar <seherkumsar99@outlook.com>
2024-03-01 23:52:03 +0300

    added hero is-primary to title

--- src/App.js
+++ src/App.js
@@ -7,18 +7,27 @@ import SiriImage from "./images/siri.png";
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistans</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistans</p>
+        </div>
+      </section>
+
       <div className="container">
         <section className="section">
           <div className="columns">
             <div className="column is-4">
-                <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
             </div>
             <div className="column is-4">
-                <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+              />
             </div>
             <div className="column is-4">
-                <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+              <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
             </div>
           </div>
         </section>
